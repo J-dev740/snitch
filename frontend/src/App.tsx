0823fbd4f932bee52ff2d3dbf8d335fd48218893
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-06-01 13:22:44 +0530

    refact: fix bugs and improve code quality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,51 @@ import { Dispatch, FormEvent, useState, DragEvent, SetStateAction } from 'react'
 import { motion } from 'framer-motion'
 import { FiPlus, FiTrash } from "react-icons/fi";
 
+enum COLUMN {
+  PENDING = "pending",
+  INPROGRESS = "inprogress",
+  COMPLETED = "completed"
+}
 
+// interface of a task card 
+
+interface tasktype {
+  id: string,
+  title: String,
+  desc?: String,
+  ttime: Date,
+  taskstate: COLUMN
+}
+// interface of column props or params
+interface cprops {
+  title: String,
+  column: COLUMN,
+  tasks: tasktype[],
+  setTasks: Dispatch<SetStateAction<tasktype[]>>
+
+}
+// interface of drop indicator 
+
+interface indicatorProps {
+  bid: string | null,
+  column: COLUMN
+}
+
+// interface of a taskcard 
+interface tprops {
+  tasks: tasktype[],
+  tsk: tasktype,
+  start: Function,
+  setTask: Dispatch<SetStateAction<tasktype[]>>
+}
+
+// interface of add task component
+interface addprops {
+  // adding: boolean
+  column: COLUMN
+  settasks: Dispatch<SetStateAction<tasktype[]>>
+}
+// main piece of code 
 function App() {
 
   return (
@@ -13,38 +57,10 @@ function App() {
 }
 
 export default App
-enum COLUMN {
-  PENDING = "pending",
-  INPROGRESS = "inprogress",
-  COMPLETED = "completed"
-}
-const dummy: tasktype[] = [
-  {
-    id: Math.random().toString(),
-    title: "go gym",
-    desc: "go to the gym",
-    ttime: new Date(),
-    taskstate: COLUMN.PENDING
-  },
-  {
-    id: Math.random().toString(),
-    title: "go football",
-    desc: "play the match",
-    ttime: new Date(),
-    taskstate: COLUMN.INPROGRESS
 
-  },
-  {
-    id: Math.random().toString(),
-    title: "play chess",
-    desc: "tournament",
-    ttime: new Date(),
-    taskstate: COLUMN.INPROGRESS
-
-  }
-]
+// this is the taskboard component enclosing the whole dynamic todo list board 
 const Taskboard = () => {
-  const [task, setTasks] = useState(dummy);
+  const [task, setTasks] = useState<tasktype[]>([]);
   return (
     <div className='flex item-center justify-center  w-full md:min-h-[500px] lg:min-h-[700px] h-full bg-white p-8 gap-3  overflow-scroll  '>
       <Column
@@ -69,24 +85,7 @@ const Taskboard = () => {
   )
 }
 
-interface tasktype {
-  id: string,
-  title: String,
-  desc?: String,
-  ttime: Date,
-  taskstate: COLUMN
-}
-interface cprops {
-  title: String,
-  column: COLUMN,
-  tasks: tasktype[],
-  setTasks: Dispatch<SetStateAction<tasktype[]>>
-
-}
-interface indicatorProps {
-  bid: string | null,
-  column: COLUMN
-}
+// this shows the drop indicator for the drop place of a particular task
 const DropIndicator = ({ bid, column }: indicatorProps) => {
   return (
     <div
@@ -96,6 +95,7 @@ const DropIndicator = ({ bid, column }: indicatorProps) => {
     </div>
   )
 }
+// column component is the compoent that is used to render the to inprogress and completed sections 
 const Column = ({ title, column, tasks, setTasks }: cprops) => {
   const [active, setActive] = useState(false);
   const handleDragOver = (e: DragEvent) => {
@@ -174,10 +174,10 @@ const Column = ({ title, column, tasks, setTasks }: cprops) => {
 
       let foundTask = tasksCopy.find((task) => task.id == taskid);
       if (!foundTask) { alert("No task found"); return; }
-      else if(foundTask.taskstate==COLUMN.COMPLETED || (foundTask.taskstate==COLUMN.INPROGRESS && column==COLUMN.PENDING)) return;
+      else if (foundTask.taskstate == COLUMN.COMPLETED || (foundTask.taskstate == COLUMN.INPROGRESS && column == COLUMN.PENDING)) return;
       else foundTask = { ...foundTask, taskstate: column }
       tasksCopy = tasksCopy.filter((task) => task.id !== taskid);
-      if(foundTask.taskstate==COLUMN.COMPLETED) foundTask.ttime=new Date(Date.now());
+      if (foundTask.taskstate == COLUMN.COMPLETED) foundTask.ttime = new Date(Date.now());
 
       // code to update the list of tasks with the updated task states;
       if (before == "-1") tasksCopy.push(foundTask);
@@ -206,7 +206,7 @@ const Column = ({ title, column, tasks, setTasks }: cprops) => {
       <div className='flex flex-row w-full h-fit justify-between  p-2 items-center mb-4 bg-stone-300 text-stone-600'>
         <div className='flex flex-row justify-center  items-center gap-2'>
           <h3 className={`flex w-fit shrink-0 font-medium`}>{title}</h3>
-          <span className='flex font-semibold tracking-wide leading-normal ml-2'>{taskfilter.length}{' '}{' '}{taskfilter.length>1?'ISSUES':'ISSUE'}</span>
+          <span className='flex font-semibold tracking-wide leading-normal ml-2'>{taskfilter.length}{' '}{' '}{taskfilter.length > 1 ? 'ISSUES' : 'ISSUE'}</span>
         </div>
       </div>
       {/* tasks list */}
@@ -224,7 +224,7 @@ const Column = ({ title, column, tasks, setTasks }: cprops) => {
           })
         }
         <DropIndicator column={column} bid={null} />
-        {column==COLUMN.PENDING && <Add  column={column} settasks={setTasks} />}
+        {column == COLUMN.PENDING && <Add column={column} settasks={setTasks} />}
 
       </div>
 
@@ -232,25 +232,19 @@ const Column = ({ title, column, tasks, setTasks }: cprops) => {
   )
 
 }
-interface tprops {
-  tasks:tasktype[],
-  tsk: tasktype,
-  start: Function,
-  setTask:Dispatch<SetStateAction<tasktype[]>>
-}
 
+// card is the template to render each task according to it's column type
+const Card = ({ tsk, start, tasks, setTask }: tprops) => {
 
-const Card = ({ tsk, start,tasks,setTask }: tprops) => {
-
-  const handleCheck=(tsk:tasktype)=>{
-    const id=tsk.id;
-    let cpy=[...tasks];
-    cpy=cpy.filter((task)=>task.id!==id);
-    let state=tsk.taskstate;
-    if(state==COLUMN.PENDING) state=COLUMN.INPROGRESS;
-    else state=COLUMN.COMPLETED;
-    const updatedtask:tasktype={...tsk,taskstate:state};
-    if(updatedtask.taskstate==COLUMN.COMPLETED) updatedtask.ttime=new Date(Date.now());
+  const handleCheck = (tsk: tasktype) => {
+    const id = tsk.id;
+    let cpy = [...tasks];
+    cpy = cpy.filter((task) => task.id !== id);
+    let state = tsk.taskstate;
+    if (state == COLUMN.PENDING) state = COLUMN.INPROGRESS;
+    else state = COLUMN.COMPLETED;
+    const updatedtask: tasktype = { ...tsk, taskstate: state };
+    if (updatedtask.taskstate == COLUMN.COMPLETED) updatedtask.ttime = new Date(Date.now());
     cpy.push(updatedtask);
     setTask(cpy);
   }
@@ -273,14 +267,13 @@ const Card = ({ tsk, start,tasks,setTask }: tprops) => {
         {tsk.desc ? (tsk.desc.length > 0 ? (<div className=' flex w-full  font-normal text-wrap text-left'>
           {tsk.desc}
         </div>) : '') : ''}
-      { tsk.taskstate!==COLUMN.COMPLETED && ( <div 
-        onClick={()=>
-          {
-            setTimeout(()=>handleCheck(tsk),300);
+        {tsk.taskstate !== COLUMN.COMPLETED && (<div
+          onClick={() => {
+            setTimeout(() => handleCheck(tsk), 300);
           }
-        }
-        className='flex w-full justify-end'>
-        <Check/>
+          }
+          className='flex w-full justify-end'>
+          <Check />
         </div>)}
         {/* timestamp */}
         {tsk.taskstate == COLUMN.COMPLETED ? (<span className='flex w-full place-content-end'>{new Date(tsk.ttime).toLocaleString().trimEnd()}</span>) : ''}
@@ -289,14 +282,11 @@ const Card = ({ tsk, start,tasks,setTask }: tprops) => {
   )
 }
 
-interface addprops {
-  // adding: boolean
-  column: COLUMN
-  settasks: Dispatch<SetStateAction<tasktype[]>>
-  // setadding: Dispatch<SetStateAction<boolean>>
-}
-const Add = ({  column, settasks }: addprops) => {
-  const [adding,setAdding]=useState(false);
+
+
+// this component lets you add a new task to the list 
+const Add = ({ column, settasks }: addprops) => {
+  const [adding, setAdding] = useState(false);
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
   const handleSubmit = (e: FormEvent) => {
@@ -368,27 +358,30 @@ const Add = ({  column, settasks }: addprops) => {
   )
 }
 
+
+
+// this is a tailwind componenet that I created to produce a seamless ux for chekbox part 
 const Check = () => {
   const [isChecked, setIsChecked] = useState(false);
 
   const handleToggle = () => {
-      setIsChecked(!isChecked);
+    setIsChecked(!isChecked);
   };
 
   return (
-      <div 
-          className={`flex items-center p-1 rounded-lg cursor-pointer transition-colors duration-300 shadow-lg ${isChecked ? 'bg-blue-500 text-white' : 'bg-gray-100'}`} 
-          onClick={handleToggle}
+    <div
+      className={`flex items-center p-1 rounded-lg cursor-pointer transition-colors duration-300 shadow-lg ${isChecked ? 'bg-blue-500 text-white' : 'bg-gray-100'}`}
+      onClick={handleToggle}
+    >
+      <div
+        className={`w-3 h-3 md:w-6 md:h-6 flex items-center justify-center border-2 rounded-md transition-colors duration-300 ${isChecked ? 'bg-white border-white' : 'bg-white border-blue-500'}`}
       >
-          <div 
-              className={`w-3 h-3 md:w-6 md:h-6 flex items-center justify-center border-2 rounded-md transition-colors duration-300 ${isChecked ? 'bg-white border-white' : 'bg-white border-blue-500'}`}
-          >
-              {isChecked && <span className="text-green-500">&#10003;</span>}
-          </div>
-          {/* <span className="ml-2 md:ml-4 font-medium">
+        {isChecked && <span className="text-green-500">&#10003;</span>}
+      </div>
+      {/* <span className="ml-2 md:ml-4 font-medium">
               {isChecked ? 'Completed' : 'Mark as Completed'}
           </span> */}
-      </div>
+    </div>
   );
 };
 
